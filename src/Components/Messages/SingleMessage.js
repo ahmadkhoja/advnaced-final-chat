8f7d90bdffbb110fbdc8a0decae3143f5afa521d
ahmadkhoja <ahmadkhoja96@gmail.com>
2018-05-09 18:52:30 +0300

    if image-->display image and if text-->display text

--- src/Components/Messages/SingleMessage.js
+++ src/Components/Messages/SingleMessage.js
@@ -1,15 +1,27 @@
 import React from 'react'
 
-const SingleMessage = ({ image, username, body, date }) => {
+const SingleMessage = ({ image, username, body, date, imagename }) => {
 
   return (
       <div className="singleMessage">
         <img src={image} alt="batata" className="messageImage" />
         <div className="messageContainer">
-          <p className="messageUsername">{username}</p>
+          <p className="messageUsername"> {username}</p>
           <p className="messageDate">{date}</p>
         </div>
-          <p className="bodyText">{body}</p>
+        {
+          body ? 
+          <div classname="message-text">
+            <p class="bodyText">{body}</p>
+          </div> : null
+        }
+        {
+          imagename ? 
+          <img className="bodyImage" src={'/uploaded_images/'+imagename} alt="batata"/> : null
+        }
+        
+         
+          
       </div>
     )
   }
